963716b6d9113042ecdffa8119bd41ff2833ecca
Unknown <pika.andraz.kralj@gmail.com>
2019-01-11 21:13:57 +0100

    Merge effects into websocket and cleanup code

--- server/vis/static/main_view/controller.js
+++ server/vis/static/main_view/controller.js
@@ -2,6 +2,7 @@ var webActionController = WebActionController();
 var scene = null;
 var camera = null;
 var renderer = null;
+var effect = null;
 
 var drone_model = null;
 var color = "#009933";  // Default same as graphs
@@ -13,191 +14,22 @@ $(function () {
     source_url = static_url + "main_view/";
     source_url = static_url + "main_view/";
 
-    scene = new THREE.Scene();
-    camera = new THREE.PerspectiveCamera(75, 1, 0.1, 1000);
-    scene.background = new THREE.Color(0x000000);
-    camera.position.z = 10;
-    var tick = 0,
-        clock = new THREE.Clock(true),
-        options,
-        spawnerOptions,
-        particleSystem;
+    // Is called when socket receives new data
+    var onNewData = function (data) {
+        console.log(data);
+    };
 
     setupGraphs();
     setupSelects();
     setupThree();
-    particleSystem = new THREE.GPUParticleSystem({
-        maxParticles: 250000
-    });
-
-    scene.add(particleSystem);
-    options = {
-        position: new THREE.Vector3(),
-        positionRandomness: 3,
-        velocity: new THREE.Vector3(),
-        velocityRandomness: 5,
-        color: 0xaa88ff,
-        colorRandomness: 10,
-        turbulence: 10,
-        lifetime: 10,
-        size: 20,
-        sizeRandomness: 100
-    };
-
-    spawnerOptions = {
-        spawnRate: 1000,
-        horizontalSpeed:1,
-        verticalSpeed: 1,
-        timeScale: 0.1
-    }
-    var renderer = new THREE.WebGLRenderer();
-    var container = document.getElementById('canvas');
-    var positionInfo = container.getBoundingClientRect();
-
-    // Is called when socket receives new data
-    var onNewData = function (data) {
-        console.log(data);
-    renderer.setSize(positionInfo.width - 2, positionInfo.height - 2);
-    renderer.shadowMap.enabled = true;
-    renderer.shadowMap.type = THREE.PCFShadowMap;
-    container.appendChild(renderer.domElement);
-
-    var data = [
-        {
-            x: [0, 1, 2, 3, 4, 5, 6, 7, 8],
-            y: [0, 1, 2, 3, 4, 5, 6, 7, 8],
-            type: 'scatter',
-            marker: {
-                color: '#009933',
-                line: {
-                    width: 2.5
-                }
-            }
-        }
-    ];
-
-    var layout = {
-        paper_bgcolor: '#00000000',
-        plot_bgcolor: '#00000000',
-        xaxis: {
-            showgrid: true,
-            mirror: 'ticks',
-            tickfont: {
-                color: 'snow'
-            },
-            gridcolor: 'snow',
-            zerolinecolor: 'snow',
-            gridwidth: 1,
-            title: {
-                text: 'X',
-                font: {
-                    color: 'snow',
-                    size: 16
-                }
-            }
-        },
-        yaxis: {
-            showgrid: true,
-            mirror: 'ticks',
-            tickfont: {
-                color: 'snow'
-            },
-            gridcolor: 'snow',
-            zerolinecolor: 'snow',
-            gridwidth: 2,
-            title: {
-                text: 'Y',
-                font: {
-                    color: 'snow',
-                    size: 16
-                }
-            }
-        },
-        title: {
-            text: 'Some title',
-            font: {
-                color: '#ffffff',
-                size: 16
-            }
-        },
-        margin: {
-            l: 50,
-            r: 25,
-            b: 50,
-            t: 50
-        }
-
-    };
-    Plotly.newPlot('tester1', data, layout, { displayModeBar: false });
-    Plotly.newPlot('tester', data, layout, { displayModeBar: false });
-    var cube2 = new THREE.Mesh(
-        new THREE.BoxGeometry(10, 1, 10),
-        new THREE.MeshLambertMaterial({ color: 0x0d0d0d })
-    );
-
-    cube2.castShadow = true;//obvezno za vsak objekt!
-    cube2.receiveShadow = true;//obvezno za vsak objekt!
-
-    cube2.position.y = -3;
-    scene.add(cube2);
-
-    scene.add(new THREE.AmbientLight(0xffffff, 0.2));
-    {//luč, najbolje directional light (sonce), najmanj težav
-        var light = new THREE.DirectionalLight(0xffffff, 0.5);
-        light.castShadow = true;
-        light.position.set(0, 10, 0);
-        light.shadow.camera.near = 0.1;
-        light.shadow.camera.far = 1000;
-
-        light.shadow.mapSize.width = 512;
-        light.shadow.mapSize.height = 512;
-
-        scene.add(light);
-    }
-    var effect = new THREE.AnaglyphEffect(renderer);
-    effect.setSize(container.offsetWidth, container.offsetHeight);
-
-    var update = function () {
-        drone_model.rotation.x += 0.01;
-        drone_model.rotation.y += 0.01;
-    };
     webActionController.setupWebSocket(onNewData);  // Connect to server
-
 });
-    //draw scene
-    var render = function () {
-        effect.render(scene, camera);
-    };
 
 $( window ).resize(function() {
     // Re init UI for new size
     setupGraphs();
     updateThree();
-    //run game loop (update, render, repeat)
-    var GameLoop = function () {
-        requestAnimationFrame(GameLoop);
-        var delta = clock.getDelta() * spawnerOptions.timeScale;
-        tick += delta;
-
-        if (tick < 0) tick = 0;
-
-        if (delta > 0) {
-            options.position.x = Math.sin(tick * spawnerOptions.horizontalSpeed) * 10;
-            options.position.y = Math.sin(tick * spawnerOptions.verticalSpeed) * 10;
-            options.position.z = Math.sin(tick * spawnerOptions.horizontalSpeed + spawnerOptions.verticalSpeed) * 2;
-
-            for (var x = 0; x < spawnerOptions.spawnRate * delta; x++) {
-                particleSystem.spawnParticle(options);
-            }
-        }
-
-        particleSystem.update(tick);
-        update();
-        render();
-    };
-
     setupSelects();
-    GameLoop();
 });
 
 // Loads a new model as "the drone"
@@ -208,16 +40,19 @@ function loadModel(obj)
 
     var objLoader = new THREE.OBJLoader();
     drone_model = objLoader.parse(obj);
+    // Enable shadows
     drone_model.name = "drone";
     drone_model.traverse(function (child) {
         if (child instanceof THREE.Mesh) {
             child.castShadow = true;
         }
     });
-    scene.add(drone_model);
+
+    // Add to scene, set position and load correct texture
     drone_model.position.z = 0;
     drone_model.position.x = 0;
     drone_model.position.y = 0;
+    scene.add(drone_model);
     loadTexture(color)
 }
 // Loads new rgb onto "the drone" model
@@ -341,39 +176,97 @@ function setupThree()
     renderer = new THREE.WebGLRenderer();
     var container = document.getElementById('canvas');
     var positionInfo = container.getBoundingClientRect();
+    var tick = 0;
+    var clock = new THREE.Clock(true);
+    var options = {
+        position: new THREE.Vector3(),
+        positionRandomness: 3,
+        velocity: new THREE.Vector3(),
+        velocityRandomness: 5,
+        color: 0xaa88ff,
+        colorRandomness: 10,
+        turbulence: 10,
+        lifetime: 10,
+        size: 20,
+        sizeRandomness: 100
+    };
+    var spawnerOptions = {
+        spawnRate: 1000,
+        horizontalSpeed:1,
+        verticalSpeed: 1,
+        timeScale: 0.1
+    };
+    var particleSystem = new THREE.GPUParticleSystem({
+        maxParticles: 250000
+    });
+    scene.add(particleSystem);
 
     // Resize correctly
     renderer.setSize(positionInfo.width-2, positionInfo.height-2);
     container.appendChild(renderer.domElement);
 
     // Create lights and add to scene
-    var keyLight = new THREE.DirectionalLight(new THREE.Color('hsl(30, 100%, 75%)'), 1.0);
-    keyLight.position.set(-100, 0, 100);
+    scene.add(new THREE.AmbientLight(0xffffff, 0.2));
+    // Best light is directional for best effects with less work
+    var light = new THREE.DirectionalLight(0xffffff, 0.5);
+    light.castShadow = true;
+    light.position.set(0, 10, 0);
+    light.shadow.camera.near = 0.1;
+    light.shadow.camera.far = 1000;
+    light.shadow.mapSize.width = 512;
+    light.shadow.mapSize.height = 512;
+    scene.add(light);
 
-    var fillLight = new THREE.DirectionalLight(new THREE.Color('hsl(240, 100%, 75%)'), 0.75);
-    fillLight.position.set(100, 0, 100);
+    // Create floor
+    var floor = new THREE.Mesh(
+        new THREE.BoxGeometry(100, 1, 15), new THREE.MeshLambertMaterial({ color: 0x0d0d0d }));
+    // All objects need to cast and receive shadows
+    floor.castShadow = true;
+    floor.receiveShadow = true;
+    floor.position.y = -5;
+    scene.add(floor);
 
-    var backLight = new THREE.DirectionalLight(0xffffff, 1.0);
-    backLight.position.set(100, 0, -100).normalize();
+    // Set renderer size
+    renderer.setSize(positionInfo.width - 2, positionInfo.height - 2);
+    renderer.shadowMap.enabled = true;
+    renderer.shadowMap.type = THREE.PCFShadowMap;
+    container.appendChild(renderer.domElement);
 
-    scene.add(keyLight);
-    scene.add(fillLight);
-    scene.add(backLight);
+    // Create effect
+    effect = new THREE.AnaglyphEffect(renderer);
+    effect.setSize(container.offsetWidth, container.offsetHeight);
 
     // Change rotation of model correctly
     var update = function () {
-        scene.rotation.x += 0.01;
-        scene.rotation.y += 0.01;
+        drone_model.rotation.x += 0.01;
+        drone_model.rotation.y += 0.01;
     };
 
-    //draw scene
+    //draw scene with effect
     var render = function () {
-        renderer.render(scene, camera);
+        effect.render(scene, camera);
     };
 
-    //run game loop (update, render, repeat)
+    //run game loop (update, render, repeat) and update particle system clock
     var GameLoop = function () {
         requestAnimationFrame(GameLoop);
+
+        var delta = clock.getDelta() * spawnerOptions.timeScale;
+        tick += delta;
+
+        if (tick < 0) tick = 0;
+
+        if (delta > 0) {
+            options.position.x = Math.sin(tick * spawnerOptions.horizontalSpeed) * 10;
+            options.position.y = Math.sin(tick * spawnerOptions.verticalSpeed) * 10;
+            options.position.z = Math.sin(tick * spawnerOptions.horizontalSpeed + spawnerOptions.verticalSpeed) * 2;
+
+            for (var x = 0; x < spawnerOptions.spawnRate * delta; x++) {
+                particleSystem.spawnParticle(options);
+            }
+        }
+
+        particleSystem.update(tick);
         update();
         render();
     };
@@ -387,4 +280,5 @@ function updateThree()
     var container = document.getElementById('canvas');
     var positionInfo = container.getBoundingClientRect();
     renderer.setSize(positionInfo.width-2, positionInfo.height-2);
+    effect.setSize(container.offsetWidth, container.offsetHeight);
 }
