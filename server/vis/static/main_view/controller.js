3c1ce6f0f749288015ce53a266b82d69a9d5c798
ASkupek <andraz.askupek@gmail.com>
2019-01-11 20:36:25 +0100

    Add effects and some threejs files

--- server/vis/static/main_view/controller.js
+++ server/vis/static/main_view/controller.js
@@ -12,112 +12,171 @@ $(function () {
 
     scene = new THREE.Scene();
     camera = new THREE.PerspectiveCamera(75, 1, 0.1, 1000);
-    scene.background = new THREE.Color( 0x000000 );
+    scene.background = new THREE.Color(0x000000);
     camera.position.z = 10;
+    var tick = 0,
+        clock = new THREE.Clock(true),
+        options,
+        spawnerOptions,
+        particleSystem;
 
+    particleSystem = new THREE.GPUParticleSystem({
+        maxParticles: 250000
+    });
+
+    scene.add(particleSystem);
+    options = {
+        position: new THREE.Vector3(),
+        positionRandomness: 3,
+        velocity: new THREE.Vector3(),
+        velocityRandomness: 5,
+        color: 0xaa88ff,
+        colorRandomness: 10,
+        turbulence: 10,
+        lifetime: 10,
+        size: 20,
+        sizeRandomness: 100
+    };
+
+    spawnerOptions = {
+        spawnRate: 1000,
+        horizontalSpeed:1,
+        verticalSpeed: 1,
+        timeScale: 0.1
+    }
     var renderer = new THREE.WebGLRenderer();
     var container = document.getElementById('canvas');
     var positionInfo = container.getBoundingClientRect();
 
-    renderer.setSize(positionInfo.width-2, positionInfo.height-2);
+    renderer.setSize(positionInfo.width - 2, positionInfo.height - 2);
+    renderer.shadowMap.enabled = true;
+    renderer.shadowMap.type = THREE.PCFShadowMap;
     container.appendChild(renderer.domElement);
 
-        var data = [
-            {
-              x: [0, 1, 2, 3, 4, 5, 6, 7, 8],
-              y: [0, 1, 2, 3, 4, 5, 6, 7, 8],
-              type: 'scatter',
-              marker: {
+    var data = [
+        {
+            x: [0, 1, 2, 3, 4, 5, 6, 7, 8],
+            y: [0, 1, 2, 3, 4, 5, 6, 7, 8],
+            type: 'scatter',
+            marker: {
                 color: '#009933',
                 line: {
                     width: 2.5
                 }
             }
-            }
-          ];
+        }
+    ];
 
-        var layout = {
-            paper_bgcolor: '#00000000',
-            plot_bgcolor: '#00000000',
-            xaxis: {
-                showgrid: true,
-                mirror: 'ticks',
-                tickfont: {
-                    color: 'snow'
-                    },
-                gridcolor: 'snow',
-                zerolinecolor: 'snow',
-                gridwidth: 1,
-                title: {
-                    text: 'X',
-                    font: {
-                        color: 'snow',
-                        size: 16
-                    }
-              }
+    var layout = {
+        paper_bgcolor: '#00000000',
+        plot_bgcolor: '#00000000',
+        xaxis: {
+            showgrid: true,
+            mirror: 'ticks',
+            tickfont: {
+                color: 'snow'
             },
-            yaxis: {
-                showgrid: true,
-                mirror: 'ticks',
-                tickfont: {
-                    color: 'snow'
-                    },
-                gridcolor: 'snow',
-                zerolinecolor: 'snow',
-                gridwidth: 2,
-                title: {
-                    text: 'Y',
-                    font: {
-                      color: 'snow',
-                      size: 16
-                    }
-                }
-            },
+            gridcolor: 'snow',
+            zerolinecolor: 'snow',
+            gridwidth: 1,
             title: {
-                text: 'Some title',
+                text: 'X',
                 font: {
-                    color: '#ffffff',
+                    color: 'snow',
                     size: 16
                 }
-            },
-            margin: {
-              l: 50,
-              r: 25,
-              b: 50,
-              t: 50
             }
+        },
+        yaxis: {
+            showgrid: true,
+            mirror: 'ticks',
+            tickfont: {
+                color: 'snow'
+            },
+            gridcolor: 'snow',
+            zerolinecolor: 'snow',
+            gridwidth: 2,
+            title: {
+                text: 'Y',
+                font: {
+                    color: 'snow',
+                    size: 16
+                }
+            }
+        },
+        title: {
+            text: 'Some title',
+            font: {
+                color: '#ffffff',
+                size: 16
+            }
+        },
+        margin: {
+            l: 50,
+            r: 25,
+            b: 50,
+            t: 50
+        }
 
-          };
-    Plotly.newPlot('tester1', data, layout, {displayModeBar: false});
-    Plotly.newPlot('tester', data, layout, {displayModeBar: false});
+    };
+    Plotly.newPlot('tester1', data, layout, { displayModeBar: false });
+    Plotly.newPlot('tester', data, layout, { displayModeBar: false });
+    var cube2 = new THREE.Mesh(
+        new THREE.BoxGeometry(10, 1, 10),
+        new THREE.MeshLambertMaterial({ color: 0x0d0d0d })
+    );
 
+    cube2.castShadow = true;//obvezno za vsak objekt!
+    cube2.receiveShadow = true;//obvezno za vsak objekt!
 
-    var keyLight = new THREE.DirectionalLight(new THREE.Color('hsl(30, 100%, 75%)'), 1.0);
-    keyLight.position.set(-100, 0, 100);
+    cube2.position.y = -3;
+    scene.add(cube2);
 
-    var fillLight = new THREE.DirectionalLight(new THREE.Color('hsl(240, 100%, 75%)'), 0.75);
-    fillLight.position.set(100, 0, 100);
+    scene.add(new THREE.AmbientLight(0xffffff, 0.2));
+    {//luč, najbolje directional light (sonce), najmanj težav
+        var light = new THREE.DirectionalLight(0xffffff, 0.5);
+        light.castShadow = true;
+        light.position.set(0, 10, 0);
+        light.shadow.camera.near = 0.1;
+        light.shadow.camera.far = 1000;
 
-    var backLight = new THREE.DirectionalLight(0xffffff, 1.0);
-    backLight.position.set(100, 0, -100).normalize();
+        light.shadow.mapSize.width = 512;
+        light.shadow.mapSize.height = 512;
 
-    scene.add(keyLight);
-    scene.add(fillLight);
-    scene.add(backLight);
+        scene.add(light);
+    }
+    var effect = new THREE.AnaglyphEffect(renderer);
+    effect.setSize(container.offsetWidth, container.offsetHeight);
 
     var update = function () {
-        scene.rotation.x += 0.01;
-        scene.rotation.y += 0.01;
+        drone_model.rotation.x += 0.01;
+        drone_model.rotation.y += 0.01;
     };
 
     //draw scene
     var render = function () {
-        renderer.render(scene, camera);
+        effect.render(scene, camera);
     };
 
     //run game loop (update, render, repeat)
     var GameLoop = function () {
         requestAnimationFrame(GameLoop);
+        var delta = clock.getDelta() * spawnerOptions.timeScale;
+        tick += delta;
+
+        if (tick < 0) tick = 0;
+
+        if (delta > 0) {
+            options.position.x = Math.sin(tick * spawnerOptions.horizontalSpeed) * 10;
+            options.position.y = Math.sin(tick * spawnerOptions.verticalSpeed) * 10;
+            options.position.z = Math.sin(tick * spawnerOptions.horizontalSpeed + spawnerOptions.verticalSpeed) * 2;
+
+            for (var x = 0; x < spawnerOptions.spawnRate * delta; x++) {
+                particleSystem.spawnParticle(options);
+            }
+        }
+
+        particleSystem.update(tick);
         update();
         render();
     };
@@ -127,57 +186,52 @@ $(function () {
 });
 
 
-function loadModel(obj)
-{
+function loadModel(obj) {
     if (drone_model)
-        scene.remove( drone_model );
+        scene.remove(drone_model);
 
     var objLoader = new THREE.OBJLoader();
     drone_model = objLoader.parse(obj);
     drone_model.name = "drone";
-
+    drone_model.traverse(function (child) {
+        if (child instanceof THREE.Mesh) {
+            child.castShadow = true;
+        }
+    });
     scene.add(drone_model);
     drone_model.position.z = 0;
     drone_model.position.x = 0;
     drone_model.position.y = 0;
     loadTexture(color)
 }
-function loadTexture(newColor)
-{
+function loadTexture(newColor) {
     color = newColor
     var selectedObject = scene.getObjectByName("drone");
-    if(selectedObject)
-        selectedObject.traverse(function(child)
-        {
-            if (child instanceof THREE.Mesh)
-            {
-                child.material = new THREE.MeshPhongMaterial( { color: color } );
+    if (selectedObject)
+        selectedObject.traverse(function (child) {
+            if (child instanceof THREE.Mesh) {
+                child.material = new THREE.MeshPhongMaterial({ color: color });
             }
         });
 }
 
 
-function setupSelects()
-{
+function setupSelects() {
     // To style only selects with the selectpicker class
     $('.selectpicker').selectpicker();
     // Add event to select element
     $('#object_select').on('changed.bs.select', function (e, clickedIndex, isSelected, previousValue) {
-        webActionController.getModel(e.target[clickedIndex].value).done(function(response)
-        {
-            if(response.status == 0)
-            {
+        webActionController.getModel(e.target[clickedIndex].value).done(function (response) {
+            if (response.status == 0) {
                 loadModel(response.data)
             }
         });
     });
     // Select first element by default
     var options = document.getElementById("object_select").options;
-    if(options.length > 0) {
-        webActionController.getModel(options[0].value).done(function(response)
-        {
-            if(response.status == 0)
-            {
+    if (options.length > 0) {
+        webActionController.getModel(options[0].value).done(function (response) {
+            if (response.status == 0) {
                 loadModel(response.data)
             }
         });
