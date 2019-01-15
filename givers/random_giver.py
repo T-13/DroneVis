1c875c6c5aa1af6d74c30ab82f7aea6de716a57d
jonpas <jonpas33@gmail.com>
2019-01-15 01:11:25 +0100

    Fix random giver data bounds

--- givers/random_giver.py
+++ givers/random_giver.py
@@ -11,19 +11,19 @@ class RandomGiver(giver.Giver):
         data = {
             "online": True,
             "armed": False,
-            "roll": random.uniform(0.0, 360.0),
-            "pitch": random.uniform(0.0, 360.0),
-            "yaw": random.uniform(0.0, 360.0),
+            "roll": random.uniform(-3.14, 3.14),
+            "pitch": random.uniform(-3.14, 3.14),
+            "yaw": random.uniform(0.0, 6.28),
             "heading": random.uniform(0.0, 360.0),
             "throttle": random.uniform(0.0, 100.0),
-            "rc_ch1": random.uniform(0.0, 3000.0),
-            "rc_ch2": random.uniform(0.0, 3000.0),
-            "rc_ch3": random.uniform(0.0, 3000.0),
-            "rc_ch4": random.uniform(0.0, 3000.0),
-            "rc_ch5": random.uniform(0.0, 3000.0),
-            "rc_ch6": random.uniform(0.0, 3000.0),
-            "rc_ch7": random.uniform(0.0, 3000.0),
-            "rc_ch8": random.uniform(0.0, 3000.0)
+            "rc_ch1": random.uniform(1000.0, 2000.0),
+            "rc_ch2": random.uniform(1000.0, 2000.0),
+            "rc_ch3": random.uniform(1000.0, 2000.0),
+            "rc_ch4": random.uniform(1000.0, 2000.0),
+            "rc_ch5": random.uniform(1000.0, 2000.0),
+            "rc_ch6": random.uniform(1000.0, 2000.0),
+            "rc_ch7": random.uniform(1000.0, 2000.0),
+            "rc_ch8": random.uniform(1000.0, 2000.0)
         }
 
         return data
